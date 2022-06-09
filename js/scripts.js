095e0ba44a8dfdb1a66a7f0a8ced036019812f69
Niko Rivas <rivasniko@gmail.com>
2022-06-08 19:49:42 -0700

    create division function in JS

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,25 @@
   //return number1 + number2;
 //}
 
-//const number1 = prompt("enter a number!");
-//const number2 = prompt("enter another number!");
+// const number1 = prompt("enter a number!");
+// const number2 = prompt("enter another number!");
 
 //window.alert(add(number1, number2));
 
-function multiply (num1, num2) {
-  return num1 * num2;
-}
+//function multiply (num1, num2) {
+  //return num1 * num2;
+//}
 
-const num1 = prompt("enter a number here!");
-const num2 = prompt("enter another number");
+// const num1 = prompt("enter a number here!");
+// const num2 = prompt("enter another number");
 
-window.alert(multiply(num1,num2));
+//window.alert(multiply(num1,num2));
+
+//function divide (num1, num2) {
+  //return num1 / num2;
+//}
+
+//const num1 = prompt("enter a number!");
+//const num2 = prompt("enter another number!");
+
+//window.alert(divide(num1,num2));
