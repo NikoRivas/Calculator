06d79767b07fab5bc1a2e60902e8f21f4c0c057f
Niko Rivas <rivasniko@gmail.com>
2022-06-08 19:26:38 -0700

    create calculator file tree

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// function saySomething(whatToSay) {
+//  window.alert(whatToSay);
+//}
+
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+const number1 = parseInt(prompt("Give me a number"));
+const number2 = parseInt(prompt("give me another number!"));
+
+window.alert(add(number1,number2));
